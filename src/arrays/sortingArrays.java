aee0328eb2f987675bad5e38be70237033231d5f
saiprakashgorti <saiprakash.vnit@gmail.com>
2022-01-16 03:21:47 +0530

    Push from terminal

--- src/arrays/sortingArrays.java
+++ src/arrays/sortingArrays.java
@@ -4,5 +4,6 @@ public class sortingArrays {
     public static void main(String[] args) {
         System.out.println("From inside sortingArrays");
         System.out.println("Another commit just to check");
+        System.out.println("git repo code - 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");
     }
 }
