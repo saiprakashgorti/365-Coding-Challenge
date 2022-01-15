519fa38b92b161e100b95086aea775265e766b10
saiprakashgorti <saiprakash.vnit@gmail.com>
2022-01-16 03:15:53 +0530

    Push from terminal

--- src/arrays/sortingArrays.java
+++ src/arrays/sortingArrays.java
@@ -3,5 +3,6 @@ package arrays;
 public class sortingArrays {
     public static void main(String[] args) {
         System.out.println("From inside sortingArrays");
+        System.out.println("Another commit just to check");
     }
 }
